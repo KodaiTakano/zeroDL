4b52469ecc686617576a8c88fa01557f579a2b2a
KodaiTakano <takano_x_kodai@icloud.com>
2021-12-29 18:43:06 +0900

    MNISTデータの読み込み

--- chapter3/mnist_data.py
+++ chapter3/mnist_data.py
@@ -1,7 +1,5 @@
-
-import sys
-import os
-sys.path.append(os.pardir)
+import sys, os
+sys.path.append(os.pardir) # os.pardir = ../
 from dataset.mnist import load_mnist
 
 (x_train, t_train), (x_test, t_test) = load_mnist(normalize=False, flatten=True)
