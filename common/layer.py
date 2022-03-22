acc67136e70157500a7d72003febf2837927efbc
KodaiTakano <takano_x_kodai@icloud.com>
2022-03-23 08:42:23 +0900

    アフィンレイヤ、softmax-wit-Lossレイヤの実装

--- common/layer.py
+++ common/layer.py
@@ -1,3 +1,6 @@
+import numpy as np
+from functions import softmax, cross_entropy_error
+
 # maskという変数は、True,FalseからなるNumpy行列で、xの要素で0以下の場所をTrue、それ以外をFalseとして保持する
 class Relu:
     def __init__(self):
@@ -14,8 +17,6 @@ class Relu:
         dout[self.mask] = 0
         dx = dout
         return dx
-
-import numpy as np
     
 class Sigmoid:
     def __init__(self):
@@ -30,3 +31,41 @@ class Sigmoid:
         dx = dout * (1.0 - self.out) * self.out
         return dx
 
+# Affine:順伝播で行う行列の積(Y = X * W + B)のこと
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+    
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+    
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.X.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
+
+# 出力層(ソフトマックス関数)と損失関数(交差エントロピー誤差)
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None # 損失(L) 
+        self.y = None # softmaxの出力
+        self.t = None # 教師データ(one-hot vector)
+        
+    def forward(self, x, t):
+        self.t  = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+        
+    def backward(self, dout = 1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size     
+        return dx
+    
